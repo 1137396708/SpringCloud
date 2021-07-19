57df112963a955ee65acc5721329072271721259
crh <crh@ngarihealth.com>
2021-07-19 09:16:54 +0800

    task# 注释

--- seata-order-service2001/src/main/java/com/at/cloud/service/impl/OrderServiceImpl.java
+++ seata-order-service2001/src/main/java/com/at/cloud/service/impl/OrderServiceImpl.java
@@ -34,7 +34,7 @@ public class OrderServiceImpl implements OrderService {
     public void create(Order order) {
         log.info("------------->开始新建订单");
         orderDao.create(order);
-
+//
         log.info("------------->订单微服务开始调用库存,做扣减 数量");
         storageService.decrease(order.getProductId(), order.getCount());
         log.info("------------->订单微服务开始调用库存,做扣减end");
